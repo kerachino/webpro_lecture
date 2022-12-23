319dfe9049299f55a9c29f5da43706873d851067
tetucame <92501955+tetucame@users.noreply.github.com>
2022-12-24 01:03:00 +0900

    Delete webpExcercise10 directory

--- webpExcercise10/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import "bulma/css/bulma.css";
-import "./style.css";
-
-import { createRoot } from "react-dom";
-import App from "./App";
-
-
-createRoot(document.querySelector("#content")).render(<App />);
-//createRoot(document.querySelector("#content2")).render(<Box />);
